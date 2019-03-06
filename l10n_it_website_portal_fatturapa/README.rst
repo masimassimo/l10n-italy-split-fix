28fb91ae273776a6a82882d3fd02117a408b6897
Simone <simone.rubino88@gmail.com>
2019-03-06 18:58:22 +0100

    Remove is_pa and ipa_code, add reason in roadmap

--- l10n_it_website_portal_fatturapa/README.rst
+++ l10n_it_website_portal_fatturapa/README.rst
@@ -32,6 +32,11 @@ Allow frontend users to edit any necessary field for Electronic invoice from the
 .. contents::
    :local:
 
+Known issues / Roadmap
+======================
+
+Fields `is_pa` and `ipa_code` are not shown in the frontend because Odoo core always supposes that frontend users are individual, and these fields are only meaningful for companies.
+
 Bug Tracker
 ===========
 
--- l10n_it_website_portal_fatturapa/controllers/main.py
+++ l10n_it_website_portal_fatturapa/controllers/main.py
@@ -8,7 +8,7 @@ from odoo.addons.l10n_it_website_portal_fiscalcode.controllers.main import \
 from odoo.tools.translate import _
 
 FATTURAPA_PORTAL_FIELDS = \
-    ['is_pa', 'ipa_code', 'codice_destinatario', 'firstname', 'lastname',
+    ['codice_destinatario', 'firstname', 'lastname',
      'pec_destinatario', 'country_id', 'fiscalcode', 'zipcode', 'vat']
 WebsitePortalFiscalCode.OPTIONAL_BILLING_FIELDS.extend(FATTURAPA_PORTAL_FIELDS)
 
