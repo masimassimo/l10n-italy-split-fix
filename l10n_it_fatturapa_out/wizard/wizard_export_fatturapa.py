OCA/l10n-italy

FIX l10n_it_fatturapa_out exporting REA data: if StatoLiquidazione is not valorized, don't export IscrizioneREA

l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -290,17 +290,20 @@ class WizardExportFatturapa(models.TransientModel):
 
     def _setRea(self, CedentePrestatore, company):
 
-        if company.fatturapa_rea_office and company.fatturapa_rea_number:
+        if (
+            company.fatturapa_rea_office and company.fatturapa_rea_number and
+            company.company.fatturapa_rea_liquidation
+        ):
+            # The required fields for IscrizioneREA (not required) are
+            # Ufficio, NumeroREA and StatoLiquidazione
             CedentePrestatore.IscrizioneREA = IscrizioneREAType(
-                Ufficio=(
-                    company.fatturapa_rea_office and
-                    company.fatturapa_rea_office.code or None),
-                NumeroREA=company.fatturapa_rea_number or None,
+                Ufficio=(company.fatturapa_rea_office.code or None),
+                NumeroREA=company.fatturapa_rea_number,
                 CapitaleSociale=(
                     company.fatturapa_rea_capital and
                     '%.2f' % company.fatturapa_rea_capital or None),
                 SocioUnico=(company.fatturapa_rea_partner or None),
-                StatoLiquidazione=company.fatturapa_rea_liquidation or None
+                StatoLiquidazione=company.fatturapa_rea_liquidation
                 )
 
     def _setContatti(self, CedentePrestatore, company):

masimassimo/l10n-italy-split-fix

l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -294,16 +294,20 @@ class WizardExportFatturapa(models.TransientModel):
 
     def _setRea(self, CedentePrestatore, company):
 
-        if company.rea_office and company.rea_code:
+        if (
+            company.rea_office and company.rea_code and
+            company.rea_liquidation_state
+        ):
+            # The required fields for IscrizioneREA (not required) are
+            # Ufficio, NumeroREA and StatoLiquidazione
             CedentePrestatore.IscrizioneREA = IscrizioneREAType(
-                Ufficio=(
-                    company.rea_office and company.rea_office.code or None),
-                NumeroREA=company.rea_code or None,
+                Ufficio=(company.rea_office.code or None),
+                NumeroREA=company.rea_code,
                 CapitaleSociale=(
                     company.rea_capital and
                     '%.2f' % company.rea_capital or None),
                 SocioUnico=(company.rea_member_type or None),
-                StatoLiquidazione=company.rea_liquidation_state or None
+                StatoLiquidazione=company.rea_liquidation_state
                 )
 
     def _setContatti(self, CedentePrestatore, company):