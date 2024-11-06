eLBati/l10n-italy

[FIX] Change move_type if all lines are negative and fiscal_document is TD01

l10n_it_fatturapa_in/models/account.py
@@ -323,7 +323,9 @@ class AccountInvoice(models.Model):
         for line in self.invoice_line_ids:
             if line.price_unit >= 0:
                 return
-        # if every line is negative, change them all
+        # if every line is negative, change them all, and change move type
+        if self.fiscal_document_type_id.code == "TD01":
+            self.move_type = "in_refund"
         for line in self.invoice_line_ids:
             line = line.with_context(check_move_validity=False)
             line.update({"price_unit": -line.price_unit})

masimassimo/l10n-italy-split-fix

l10n_it_fatturapa_in/models/account.py
@@ -343,7 +343,9 @@ class AccountInvoice(models.Model):
         for line in self.invoice_line_ids:
             if line.price_unit >= 0:
                 return
-        # if every line is negative, change them all
+        # if every line is negative, change them all, and change move type
+        if self.fiscal_document_type_id.code == "TD01":
+            self.type = "in_refund"
         for line in self.invoice_line_ids:
             line.price_unit = -line.price_unit
         self.compute_taxes()