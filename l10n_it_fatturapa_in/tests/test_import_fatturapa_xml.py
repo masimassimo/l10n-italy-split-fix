OCA/l10n-italy

[FIX]l10n_it_fatturapa_in: fix import with type minimo Added check if invoice_line_ids is valorize to check the price_unit, if is not valorize stop the method because is not possibile assume the line is negative Added test to veryfy when I imported a invoice with Minimo is not converted to credit, because have not lines to check

l10n_it_fatturapa_in/tests/test_import_fatturapa_xml.py
@@ -448,6 +448,7 @@ class TestFatturaPAXMLValidation(FatturapaCommon):
         self.assertTrue(len(invoices) == 2)
         for invoice in invoices:
             self.assertTrue(len(invoice.invoice_line_ids) == 0)
+            self.assertTrue(invoice.move_type == "in_invoice")
         # allow following tests to reuse the same XML file
         invoices[0].ref = invoices[0].payment_reference = "14165"
         invoices[1].ref = invoices[1].payment_reference = "14166"

masimassimo/l10n-italy-split-fix

l10n_it_fatturapa_in/tests/test_import_fatturapa_xml.py
@@ -413,6 +413,7 @@ class TestFatturaPAXMLValidation(FatturapaCommon):
         self.assertTrue(len(invoices) == 2)
         for invoice in invoices:
             self.assertTrue(len(invoice.invoice_line_ids) == 0)
+            self.assertTrue(invoice.type == "in_invoice")
 
     def test_03_xml_import(self):
         # Testing CAdES signature